e2c7228fec067e7ba1555ebb9ec74417e628fd39
nick <40447503+nickstanley574@users.noreply.github.com>
2024-10-17 18:36:48 -0500

    Remove placeholder database urls and move SECRET_KEY to env var

--- config/config.py
+++ config/config.py
@@ -1,4 +1,6 @@
+import os
+
 class Config:
-    SECRET_KEY = "your_default_secret_key"
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = "sqlite:///todo.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
